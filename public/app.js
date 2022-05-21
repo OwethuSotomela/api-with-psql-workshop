c70e12289646979fe57c38ea764c8e60326e33d3
OwethuSotomela <owethusotomela@gmail.com>
2022-05-21 17:30:33 +0200

    Calling API from Alpine

--- public/app.js
+++ public/app.js
@@ -1,15 +1,25 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('users', () => {
+document.addEventListener("alpine:init", () => {
+    Alpine.data("users", () => {
         return {
             init() {
-                fetch('https://jsonplaceholder.typicode.com/users')
-                    .then(r => r.json())
-                    .then(userData => this.users = userData)
+                // fetch("https://jsonplaceholder.typicode.com/users")
+                //     .then((r) => r.json())
+                //     .then((userData) => (this.users = userData));
+                console.log('Hi Oz!')
             },
             open: false,
-            users: []
-        }
-    })
-})
-
+            users: [
+                {
+                    username: "OwethuSotomela",
+                },
+            ],
+            logMe() {
+                alert("Right direction");
+            },
+            getToken(){
+                alert("Right direction to get my token")
+            }
+        };
+    });
+});
 
